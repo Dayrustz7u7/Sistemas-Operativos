cc01b2a33b3127a1f6dc5e369dff22e82730a509
Jignacio14 <j14castro2001@gmail.com>
2024-05-26 16:52:31 -0300

    Agrego correcciones sobre pipes
    Agrego las correcciones sobre pipes, hasta aca siguen pasando las
    pruebas

--- shell/exec.c
+++ shell/exec.c
@@ -162,39 +162,38 @@ exec_cmd(struct cmd *cmd)
 		// Verifique si la longitud del nombre del archivo (en la
 		// estructura CMD EXEC) es mayor que cero
 		//
-	r = (struct execcmd *) cmd;
+		r = (struct execcmd *) cmd;
 
 		if (strlen(r->in_file) > 0) {
 			int fd_in =
 			        open_redir_fd(r->in_file, O_RDONLY | O_CLOEXEC);
 
-			if (dup2(fd_in, STDIN_FILENO) ==  -1) {
-				printf_debug("failed to dup stdin \n"); 
-				_exit(EXIT_FAILURE); 
+			if (dup2(fd_in, STDIN_FILENO) == -1) {
+				printf_debug("failed to dup stdin \n");
+				_exit(EXIT_FAILURE);
 			};
 
 			if (fd_in > 2) {
 				if (close(fd_in) == -1) {
-					printf_debug("failed closing file \n"); 
-					_exit(EXIT_FAILURE); 
+					printf_debug("failed closing file \n");
+					_exit(EXIT_FAILURE);
 				}
 			}
-
 		}
 		if (strlen(r->out_file) > 0) {
 			int fd_out = open_redir_fd(r->out_file,
 			                           O_CREAT | O_CLOEXEC |
 			                                   O_WRONLY | O_TRUNC);
-			
-			if (dup2(fd_out, STDOUT_FILENO) ==  -1) {
-				printf_debug("failed to dup stout \n"); 
-				_exit(EXIT_FAILURE); 
+
+			if (dup2(fd_out, STDOUT_FILENO) == -1) {
+				printf_debug("failed to dup stout \n");
+				_exit(EXIT_FAILURE);
 			};
 
 			if (fd_out > 2) {
 				if (close(fd_out) == -1) {
-					printf_debug("failed closing file \n"); 
-					_exit(EXIT_FAILURE); 
+					printf_debug("failed closing file \n");
+					_exit(EXIT_FAILURE);
 				}
 			}
 		}
@@ -204,21 +203,22 @@ exec_cmd(struct cmd *cmd)
 			} else {
 				int fd_err = open_redir_fd(r->err_file,
 				                           O_TRUNC | O_WRONLY |
-				                                   O_CREAT | O_CLOEXEC);
-				
-			
-			if (dup2(fd_err, STDERR_FILENO) ==  -1) {
-				printf_debug("failed to dup stderr \n"); 
-				_exit(EXIT_FAILURE); 
-			};
+				                                   O_CREAT |
+				                                   O_CLOEXEC);
 
-			if (fd_err > 2) {
-				if (close(fd_err) == -1) {
-					printf_debug("failed closing file \n"); 
-					_exit(EXIT_FAILURE); 
+
+				if (dup2(fd_err, STDERR_FILENO) == -1) {
+					printf_debug("failed to dup stderr \n");
+					_exit(EXIT_FAILURE);
+				};
+
+				if (fd_err > 2) {
+					if (close(fd_err) == -1) {
+						printf_debug("failed closing "
+						             "file \n");
+						_exit(EXIT_FAILURE);
+					}
 				}
-			}
-			
 			}
 		}
 		r->type = EXEC;
@@ -228,9 +228,6 @@ exec_cmd(struct cmd *cmd)
 	case PIPE: {
 		// pipes two commands
 		p = (struct pipecmd *) cmd;
-
-		// Todos los procesos hijos se llevan el mismo id
-		setpgid(0, 0);
 
 		int fd[2];
 
@@ -251,9 +248,9 @@ exec_cmd(struct cmd *cmd)
 		}
 
 		if (proceso_izq == 0) {
-			close(READ);
 			dup2(fd[WRITE], STDOUT_FILENO);
-			close(WRITE);
+			close(fd[READ]);
+			close(fd[WRITE]);
 			exec_cmd(p->leftcmd);
 			_exit(-1);
 		}
@@ -269,19 +266,21 @@ exec_cmd(struct cmd *cmd)
 		}
 
 		if (proceso_der == 0) {
-			close(fd[WRITE]);
+			setpgid(0, 0);
 			dup2(fd[READ], STDIN_FILENO);
-			close(READ);
+			close(fd[WRITE]);
+			close(fd[READ]);
 			exec_cmd(p->rightcmd);
 			_exit(-1);
 		}
 
 		close(fd[READ]);
 		close(fd[WRITE]);
+		// free(stack.ss_sp);
 		waitpid(proceso_izq, NULL, 0);
 		waitpid(proceso_der, NULL, 0);
-		free_command(cmd);
-		_exit(0);
+		free_command(parsed_pipe);
+		_exit(EXIT_SUCCESS);
 	}
 	}
 }
